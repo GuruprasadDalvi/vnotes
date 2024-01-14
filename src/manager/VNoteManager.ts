34a80aca522460c678f6c72393eb284165c0b2d5
makeitgrowGIT <makeitgrow.in@gmail.com>
2024-01-14 17:03:09 +0530

    Refactor VNoteManager to generate HTML content from JSON data

--- src/manager/VNoteManager.ts
+++ src/manager/VNoteManager.ts
@@ -34,45 +34,6 @@ export class VNoteManager {
         let vNoteData: VNoteData = VNoteData.fromJSON(content);
         let generatedHTML = vNoteData.toHTML(); 
 
-        let htmlContent = "";
-        const jsonContent = JSON.parse(content);
-        for (let i = 0; i < jsonContent.length; i++) {
-            const element = jsonContent[i];
-            if (element.type === "h1") {
-                htmlContent+= `<div class="container"> <div class="add_button">+</div><h2>${element.content}</h2></div>`;
-            }
-            else if (element.type === "h2") {
-                htmlContent+= `<div class="container"> <div class="add_button">+</div><h3>${element.content}</h3></div>`;
-            }
-            else if (element.type === "h3") {
-                htmlContent+= `<div class="container"> <div class="add_button">+</div><h4>${element.content}</h4></div>`;
-            }
-            else if (element.type === "bl") {
-                let listContent = "";
-                for (let j = 0; j < element.content.length; j++) {
-                    const item = element.content[j];
-                    listContent += `<li>${item.content}</li>`;
-                }
-                htmlContent+= `<div class="container"> <div class="add_button">+</div><ul>${listContent}</ul></div>`;
-            }
-            else if (element.type === "todoList") {
-                let listContent = "";
-                for (let j = 0; j < element.content.length; j++) {
-                    const item = element.content[j];
-                    if (item.done) {
-                        listContent += `<li class="checked"><input type="checkbox" checked  >${item.content}</li>`;
-                    }
-                    else {
-                        listContent += `<li><input type="checkbox">${item.content}</li>`;
-                    }
-                }
-                htmlContent+= `<div class="container"> <div class="add_button">+</div><ul class="todoList">${listContent}</ul></div>`;
-            }
-            else if (element.type === "text") {
-                htmlContent+= `<div class="container"> <div class="add_button">+</div><INPUT id="${element.id}" class="text"  placeholder="type '/' to enter command mode" value='${element.content}' /> <br/></div>` ;
-            }
-        }
-
         const creationDateTime = new Date(vnote.creationTimestamp).toLocaleString();
         const lastUpdatedDateTime = new Date(vnote.lastUpdatedTimestamp).toLocaleString();
         let template = `
